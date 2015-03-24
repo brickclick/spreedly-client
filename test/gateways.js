34def5a543029fa5035af7363250ec4bc91e3aaa
Kevin Smith <kevin@chilicode.ca>
2015-03-24 13:01:39 -0700

    - Updated gateways test to redact all retained gateways, instead of just the first one

--- test/gateways.js
+++ test/gateways.js
@@ -1,4 +1,5 @@
 var should = require('chai').should(),
+	Promises = require('bluebird'),
 	Spreedly = require('../index'),
 	environmentKey = process.env.ENVIRONMENT_KEY,
 	accessSecret = process.env.ACCESS_SECRET;
@@ -6,7 +7,7 @@ var should = require('chai').should(),
 var spreedlyClient = new Spreedly(environmentKey, accessSecret);
 
 describe('Spreedly', function() {
-	describe.skip('Gateway Management', function() {
+	describe('Gateway Management', function() {
 		it('Can list supported gateway types', function(done) {
 			spreedlyClient.listAvailableGateways(function(err, result){
 				should.not.exist(err);
@@ -58,21 +59,32 @@ describe('Spreedly', function() {
 			spreedlyClient.listGateways(function(err, result) {
 				should.not.exist(err);
 
-				var token = result[0].token;
-
-				spreedlyClient.redactGateway(token, function(err, result) {
-					should.not.exist(err);
-					should.exist(result);
-
-					result.should.be.an('object');
-					result.should.contain.all.keys('createdAt', 'updatedAt',
-						'succeeded', 'transactionType', 'message', 'gateway');
-					result.succeeded.should.equal(true);
-					result.transactionType.should.equal('RedactGateway');
-					result.gateway.state.should.equal('redacted');
-
-					done();
+				var promises = [];
+				result.forEach(function(gateway){
+					if(gateway.state === 'retained') {
+						promises.push(spreedlyClient.redactGateway(gateway.token));
+					}
 				});
+				// Should have one from above
+				promises.should.not.be.empty;
+
+				Promises.all(promises).then(function(result) {
+					should.exist(result);
+					result.should.be.an('array');
+					result.should.not.be.empty;
+
+					result[0].should.be.an('object');
+					result[0].should.contain.all.keys('createdAt', 'updatedAt',
+						'succeeded', 'transactionType', 'message', 'gateway');
+					result[0].succeeded.should.equal(true);
+					result[0].transactionType.should.equal('RedactGateway');
+					result[0].gateway.state.should.equal('redacted');
+
+				})
+					.catch(function(err) {
+						should.not.exist(err);
+					})
+					.finally(done);
 			});
 		});
 		it('Can get info for a single gateway', function(done) {
